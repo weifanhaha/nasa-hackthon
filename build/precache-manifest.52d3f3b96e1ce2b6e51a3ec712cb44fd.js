7e9061565eb2f2ca055e767719be1be769d9651b
weifanhaha <aily34809@gmail.com>
2019-10-19 17:41:59 +0800

    deploy

---
+++ build/precache-manifest.52d3f3b96e1ce2b6e51a3ec712cb44fd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0dc034afc5948367304283cac603198",
+    "url": "/nasa-hackthon/index.html"
+  },
+  {
+    "revision": "13c2cb908411ccbe26bc",
+    "url": "/nasa-hackthon/static/css/main.8e18de48.chunk.css"
+  },
+  {
+    "revision": "65ed3b696681b401702b",
+    "url": "/nasa-hackthon/static/js/2.a3cf7a05.chunk.js"
+  },
+  {
+    "revision": "13c2cb908411ccbe26bc",
+    "url": "/nasa-hackthon/static/js/main.cc99ee86.chunk.js"
+  },
+  {
+    "revision": "d48ad583206d6b94f46d",
+    "url": "/nasa-hackthon/static/js/runtime-main.3fcfbe6f.js"
+  },
+  {
+    "revision": "22ce2a1a71510206bb3788db4494f62f",
+    "url": "/nasa-hackthon/static/media/coin.22ce2a1a.png"
+  },
+  {
+    "revision": "0869410885fd88023951d75bd61992c4",
+    "url": "/nasa-hackthon/static/media/earth.08694108.jpg"
+  },
+  {
+    "revision": "81530af0cb66e9bbd24088e17da33e9c",
+    "url": "/nasa-hackthon/static/media/leaf.81530af0.png"
+  },
+  {
+    "revision": "35c7f9a9e5d013d71c6939efc593001c",
+    "url": "/nasa-hackthon/static/media/maldives.35c7f9a9.jpg"
+  },
+  {
+    "revision": "464b3a976df1004cc10fba5490a5527d",
+    "url": "/nasa-hackthon/static/media/typhoon.464b3a97.jpeg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.10a13083ba86a8e4d93936eeb139d34a.js"
+  "/nasa-hackthon/precache-manifest.52d3f3b96e1ce2b6e51a3ec712cb44fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nasa-hackthon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
